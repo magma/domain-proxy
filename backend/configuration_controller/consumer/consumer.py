6c0f0c79fc40d1d1802e105a3b8a440e951efbe5
Dysproz <skrasuski@freedomfi.com>
2021-04-08 10:02:14 +0200

    Add basic consumer class
    Signed-off-by: Dysproz <skrasuski@freedomfi.com>

---
+++ backend/configuration_controller/consumer/consumer.py
@@ -0,0 +1,61 @@
+import pika
+
+
+class RequestsConsumer:
+    """Consumer for RabbitMQ to receive data
+    from multiple queues for every type of request to SAS.
+
+    :param rabbitmq_host: Host address to RabbitMQ instance
+    :type rabbitmq_host: str
+    """
+    _requests = [
+        "RegistrationRequest",
+        "SpectrumInquiryRequest",
+        "GrantRequest",
+        "HeartbeatRequest",
+        "RelinquishmentRequest",
+        "DeregistrationRequest",
+    ]
+
+    def __init__(self, rabbitmq_host=None):
+        if rabbitmq_host:
+            self._host = rabbitmq_host
+        else:
+            raise Exception("No RabbitMQ host has been specified")
+        self._create_communication_channel()
+
+    def _create_communication_channel(self):
+        """This method creates communication channel to RabbitMQ
+        and declares queues for every type of requests.
+        Requests queues are based on self._requests list
+        and created channel is stored in self._channel.
+        """
+        connection_params = pika.ConnectionParameters(host=self._host)
+        connection = pika.BlockingConnection(connection_params)
+        channel = connection.channel()
+        channel.exchange_declare(exchange='topic_logs', exchange_type='topic')
+        for requestName in self._requests:
+            result = channel.queue_declare('', exclusive=True)
+            channel.queue_bind(exchange='topic_logs',
+                               queue=result.method.queue,
+                               routing_key=f'"{requestName}"')
+            channel.basic_consume(queue=result.method.queue,
+                                  on_message_callback=self._queue_callback,
+                                  auto_ack=True)
+        self._channel = channel
+
+    @staticmethod
+    def _queue_callback(ch, method, properties, body):
+        print('body')
+
+    def process_data_events(self, time_limit=1):
+        """This method process data events
+        in RabbitMQ queues for specified time limit.
+
+        All requests received from RabbitMQ are stored in... <TBD>
+
+        :param time_limit: Time limit of collecting requests
+        from RabbitMQ in seconds
+        :type time_limit: int
+        """
+        self._channel.connection.process_data_events(time_limit=time_limit)
