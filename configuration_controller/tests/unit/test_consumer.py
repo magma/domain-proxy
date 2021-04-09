6f09c269b83dd3c1a7646f04dac20fd205e1c133
Dysproz <skrasuski@freedomfi.com>
2021-04-09 13:24:57 +0200

    Add mocked test on non empty queue
    Signed-off-by: Dysproz <szymon.krasuski@codilime.com>
    Signed-off-by: Dysproz <skrasuski@freedomfi.com>

--- configuration_controller/tests/unit/test_consumer.py
+++ configuration_controller/tests/unit/test_consumer.py
@@ -1,6 +1,6 @@
 import unittest
 import mock
-
+import json
 
 from configuration_controller.consumer.consumer import RequestsConsumer
 
@@ -11,7 +11,44 @@ class TestConsumerNoHostException(unittest.TestCase):
 
 
 class TestConsumerProcessDataEvents(unittest.TestCase):
+
+    @staticmethod
+    def _mock_on_message_call(consumer):
+        requests = [
+            {'routing_key': 'RegistrationRequest',
+             'body': '{"object_number": 1}'},
+            {'routing_key': 'DeregistrationRequest',
+             'body': '{"object_number": 2}'},
+            {'routing_key': 'RegistrationRequest',
+             'body': '{"object_number": 3}'},
+        ]
+        for request in requests:
+            consumer._queue_callback(request['routing_key'], request['body'])
+
     @mock.patch('pika.BlockingConnection')
     def test_process_data_events_empty(self, mock_BlockingConnection):
         consumer = RequestsConsumer('1.1.1.1')
         self.assertIsInstance(consumer.process_data_events(), dict)
+
+    @mock.patch('pika.BlockingConnection')
+    def test_process_data_events_non_empty(self, mock_BlockingConnection):
+        consumer = RequestsConsumer('1.1.1.1')
+        consumer._channel.connection.process_data_events = mock.Mock(
+            side_effect=lambda time_limit:
+                self._mock_on_message_call(consumer))
+        received_requests = consumer.process_data_events()
+        for request_key, requests_body in received_requests.items():
+            json_body = json.loads(requests_body)
+            collected_object_numbers = []
+            for request_object in json_body:
+                collected_object_numbers.append(
+                    request_object['object_number'])
+            if request_key == 'RegistrationRequest':
+                self.assertTrue(len(json_body) == 2)
+                self.assertListEqual(collected_object_numbers, [1, 3])
+            elif request_key == 'DeregistrationRequest':
+                self.assertTrue(len(json_body) == 1)
+                self.assertListEqual(collected_object_numbers, [2])
+            else:
+                self.assertTrue(len(json_body) == 0)
+                self.assertListEqual(collected_object_numbers, [])
