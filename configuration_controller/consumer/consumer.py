52caed93726acb4d934af199389587b0cbb76c9a
Dysproz <skrasuski@freedomfi.com>
2021-04-09 13:24:57 +0200

    Add mocked test on non empty queue
    Signed-off-by: Dysproz <szymon.krasuski@codilime.com>
    Signed-off-by: Dysproz <skrasuski@freedomfi.com>

--- configuration_controller/consumer/consumer.py
+++ configuration_controller/consumer/consumer.py
@@ -23,6 +23,7 @@ class RequestsConsumer:
         else:
             raise Exception("No RabbitMQ host has been specified")
         self._create_communication_channel()
+        self._received_requests = dict()
 
     def _create_communication_channel(self):
         """This method creates communication channel to RabbitMQ
@@ -42,14 +43,12 @@ class RequestsConsumer:
             channel.basic_consume(queue=result.method.queue,
                                   on_message_callback=lambda ch,
                                   method, properties, body:
-                                  self._queue_callback(ch,
-                                                       method,
-                                                       properties,
+                                  self._queue_callback(method.routing_key,
                                                        body),
                                   auto_ack=True)
         self._channel = channel
 
-    def _queue_callback(self, ch, method, properties, body):
+    def _queue_callback(self, routing_key, body):
         """Callback method for pika channel basic_consume on_message_callback
         This method appends body to proper request string based on routing key
 
@@ -62,7 +61,9 @@ class RequestsConsumer:
         :param body: Body of the received message
         :type body: bytes
         """
-        self._received_requests[method.routing_key].append(body)
+        if isinstance(body, bytes):
+            body = body.decode('utf-8')
+        self._received_requests[routing_key] += body + ','
 
     def process_data_events(self, time_limit=1) -> dict:
         """This method process data events
@@ -78,6 +79,12 @@ class RequestsConsumer:
         of all requests collected by specific routing key
         :rtype: dict
         """
-        self._received_requests = dict.fromkeys(self._requests, '')
+        self._received_requests = dict.fromkeys(self._requests, '[')
         self._channel.connection.process_data_events(time_limit=time_limit)
+        for request in self._requests:
+            if len(self._received_requests[request]) > 1:
+                self._received_requests[request] = \
+                    self._received_requests[request][:-1] + ']'
+            else:
+                self._received_requests[request] += ']'
         return self._received_requests
