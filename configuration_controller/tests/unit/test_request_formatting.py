03fb9f75ce1e60c31f817daa2be50289a64b9c67
Wojciech Sadowy <wojciech.sadowy@freedomfi.com>
2021-04-12 15:44:16 +0200

    simplyfying request merger
    Merger does not validate the structure of the requests anymore as
    it will be the responsibility of a Prodocol / Radio Controller in future iterations
    
    Signed-off-by: Wojciech Sadowy <wojciech.sadowy@freedomfi.com>

--- configuration_controller/tests/unit/test_request_formatting.py
+++ configuration_controller/tests/unit/test_request_formatting.py
@@ -1,55 +1,47 @@
 import json
-import os
 from unittest import TestCase
 
 from parameterized import parameterized
 
-from configuration_controller.request_formatting.merger import PayloadFormatException, merge_requests
+from configuration_controller.request_formatting.merger import merge_requests
+from configuration_controller.tests.unit.fixtures.fake_request_queues.queued_requests import queued_requests
+from configuration_controller.tests.unit.fixtures.fake_request_queues.mixed_queued_requests \
+    import queued_requests as mixed_queued_requests
+from configuration_controller.tests.unit.fixtures.fake_request_queues.queued_requests_with_one_cumulative_request \
+    import queued_requests as queued_requests_with_one_cumulative_request
 
 
 class RequestMergingTestCase(TestCase):
 
-    def test_request_merging_returns_empty_json_obj_for_empty_request(self):
+    def test_request_merging_returns_empty_json_obj_for_empty_request_list(self):
         # Given / When
-        merged_requests = merge_requests('[]')
+        merged_requests = merge_requests([])
 
         # Then
         self.assertEqual('{}', merged_requests)
 
     @parameterized.expand([
-        ('queued_requests.json', 1, 2),
-        ('queued_requests_with_one_cumulative_request.json', 1, 3),
+        (queued_requests, 1, 2),
+        (queued_requests_with_one_cumulative_request, 1, 3),
     ])
-    def test_request_merging_merges_multiple_json_requests_into_one(self, fixture_file, nr_of_keys, nr_of_values):
-        # Given
-        json_requests = self._get_json_from_fixture(fixture_file)
-
-        # When
-        merged_requests = merge_requests(json_requests)
+    def test_request_merging_merges_multiple_requests_into_one(self, _requests, nr_of_keys, nr_of_elems_in_list):
+        # Given / When
+        merged_requests = merge_requests(_requests)
         deserialized_merged_requests = json.loads(merged_requests)
 
         # Then
         self.assertIsInstance(deserialized_merged_requests, dict)
         self.assertEqual(nr_of_keys, len(deserialized_merged_requests.keys()))
         self.assertIsInstance(list(deserialized_merged_requests.values())[0], list)
-        self.assertEqual(nr_of_values, len(list(deserialized_merged_requests.values())[0]))
+        self.assertEqual(nr_of_elems_in_list, len(list(deserialized_merged_requests.values())[0]))
 
-    def test_request_merging_raises_exception_for_mixed_requests_in_one_queue(self):
-        # Given
-        json_requests = self._get_json_from_fixture('mixed_queued_requests.json')
+    def test_request_merging_merges_mixed_type_of_requests_into_one(self):
+        # Given / When
+        merged_requests = merge_requests(mixed_queued_requests)
+        deserialized_merged_requests = json.loads(merged_requests)
 
-        # When / Then
-        with self.assertRaises(PayloadFormatException):
-            merge_requests(json_requests)
-
-    def test_request_merging_raises_exception_for_wrong_request_structure(self):
-        # Given
-
-        # When / Then
-        with self.assertRaises(PayloadFormatException):
-            merge_requests('foo')
-
-    @staticmethod
-    def _get_json_from_fixture(fixture_name):
-        with open(os.path.join(os.path.dirname(__file__), "fixtures/fake_request_queues", fixture_name)) as f:
-            return json.dumps(json.load(f))
+        # Then
+        self.assertEqual(2, len(deserialized_merged_requests.keys()))
+        self.assertEqual(2, len(deserialized_merged_requests.values()))
+        self.assertEqual(1, len(list(deserialized_merged_requests.values())[0]))
+        self.assertEqual(1, len(list(deserialized_merged_requests.values())[1]))
