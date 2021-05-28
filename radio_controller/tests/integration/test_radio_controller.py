c6c8f5b823a0eb65f433e1c7e4efb0a662a514ef
Marcin Trojanowski <marcin.trojanowski@freedomfi.com>
2021-05-28 14:43:33 +0200

    unit tests code coverage
    Signed-off-by: Marcin Trojanowski <marcin.trojanowski@freedomfi.com>

---
+++ radio_controller/tests/integration/test_radio_controller.py
@@ -0,0 +1,14 @@
+import unittest
+
+CORRECT_CERT_PATH = '/backend/radio_controller/certs/root_ca.cert'
+INCORRECT_CERT_PATH = '/backend/radio_controller/certs/device_b_ca.cert'
+NONEXISTENT_CERT_PATH = '/nonexistent/cert/path.cert'
+
+
+class ConfigurationControllerTestCase(unittest.TestCase):
+    """Need to write new tests once the db is in place"""
+    pass
+
+def test_placeholder():
+    """Need to write new tests once the db is in place"""
+    pass
