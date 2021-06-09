bb25f4c61b307243e2f67d21906a8f3932e417b3
Wojciech Sadowy <wojciech.sadowy@freedomfi.com>
2021-06-09 11:47:40 +0200

    organize requirements alphabetically, increase default timeout
    Signed-off-by: Wojciech Sadowy <wojciech.sadowy@freedomfi.com>

--- protocol_controller/config.py
+++ protocol_controller/config.py
@@ -6,7 +6,7 @@ class Config:
     TESTING = False
     LOG_LEVEL = os.environ.get('LOG_LEVEL', 'DEBUG')
     API_PREFIX = os.environ.get('API_PREFIX', '/sas/v1')
-    RC_RESPONSE_WAIT_TIMEOUT = int(os.environ.get('RC_RESPONSE_WAIT_TIMEOUT', 5))  # TODO Set 5 for testing only
+    RC_RESPONSE_WAIT_TIMEOUT = int(os.environ.get('RC_RESPONSE_WAIT_TIMEOUT', 60))
     RC_RESPONSE_WAIT_INTERVAL = int(os.environ.get('RC_RESPONSE_WAIT_INTERVAL', 1))
 
     # gRPC
