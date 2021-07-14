25c913792a704e98077fdd68fbbc9ae53c7818d2
Artur Dębski <artur.debski@freedomfi.com>
2021-07-14 11:53:05 +0200

    Bind cbsd_sas PC plugin to 0.0.0.0:8080
    Fixed an issue with protocol controller cbsd_sas plugin that would bind
    the service to localhost.
    
    Signed-off-by: Artur Dębski <artur.debski@freedomfi.com>

--- protocol_controller/plugins/cbsd_sas/__init__.py
+++ protocol_controller/plugins/cbsd_sas/__init__.py
@@ -4,4 +4,4 @@ from protocol_controller.plugins.cbsd_sas.wsgi import application
 
 class CBSDSASProtocolPlugin(ProtocolPlugin):
     def initialize(self):
-        application.run()
+        application.run(host='0.0.0.0', port=8080)
